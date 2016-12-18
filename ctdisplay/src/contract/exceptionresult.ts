4c4b4552cf63dc32c002f4f11b0a0dc5b954b7c6
Dariusz Parzygnat <dariusz.parzygnat@gmail.com>
2016-12-18 19:52:04 +0100

    CT-4 Changes in exception result contract

--- ctdisplay/src/contract/exceptionresult.ts
+++ ctdisplay/src/contract/exceptionresult.ts
@@ -1,3 +1,16 @@
 export class ExceptionResult {
-    constructor(private message:string, private type:string){}
+    constructor(private message:string, private errorType:string, private lineWithError: number = 0){}
+    
+    get ErrorMessage () : string{
+        return this.message;
+    }
+
+    get ErrorType () : string{
+        return this.errorType;
+    }
+
+    get LineWithError () : number {
+        return this.lineWithError;
+    }
+
 }
